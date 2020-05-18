844b77a679678ab7c42ad0c41394be34f5db4d77
Tibor Nagy <tibornagy77@gmail.com>
2020-05-18 13:11:22 +0200

    Initial commit

---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * (c) Tibor Nagy <tibornagy77@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace tcsk\HelloComposer;
+
+class Hello
+{
+    public function say($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+}
